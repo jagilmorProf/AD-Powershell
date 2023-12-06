5301633805414561bdd467b658d7d661720172bd
Jarod Gilmore <145058956+jagilmorProf@users.noreply.github.com>
2023-12-06 15:52:32 -0600

    Update Autodesk-Uninstall.ps1

--- Autodesk-Uninstall.ps1
+++ Autodesk-Uninstall.ps1
@@ -1,7 +1,8 @@
+##For use in Windows PowerShell to remove all software names that contain Autodesk
+##Use at your own Risk!
+
 $products = Get-WmiObject -Class Win32_Product | Where-Object { 
-    $_.Name -like "*Autodesk Revit*" -or
-    $_.Name -like "*Navisworks*" -or
-    $_.Name -like "*AutoCAD*"
+    $_.Name -like "*Autodesk*"
 }
 
 foreach ($product in $products) {
